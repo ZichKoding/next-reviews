e01a49177a1fcfc3e1db5da4396d46ac910c04fb
ChrisZichko <108543895+ChrisZichko@users.noreply.github.com>
2023-09-29 10:54:31 -0400

    sect. 3, lecture 14 complete - made a build and tested on a prod-like server

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\zichk\Desktop\Next Projects\next-reviews\app\page.tsx
+// File: C:\Users\tzichko\Desktop\projects\next-reviews\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 

0de48f36cab31f814f5845f7f8fd4fbfc0996e49
ChrisZichko <108543895+ChrisZichko@users.noreply.github.com>
2023-10-03 08:42:13 -0400

    Sect 8, Lect 48 - Turned the ShareLinkButton into a client component and went over hydration

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 export default function ShareLinkButton() {
     const handleClick = () => {
         console.log("Clicked!");
     };
 
+    console.log('[ShareLinkButton] rendering');
+    
     return (
         <button onClick={handleClick}
             className="border px-2 py-1 rounded text-slate-500 text-sm

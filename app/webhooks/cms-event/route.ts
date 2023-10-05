e1d2d4df58da875bd1880cf57e92d08593dd13f9
ChrisZichko <108543895+ChrisZichko@users.noreply.github.com>
2023-10-05 14:12:56 -0400

    Sect 12, Lect 80 - Added a simple Webhook in cms and in app dir... Will be adding to functionality in next lesson.

--- app/webhooks/cms-event/route.ts
+++ app/webhooks/cms-event/route.ts
@@ -2,6 +2,6 @@ import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
     const payload = await request.json();
-    console.log(payload);
+    console.log('payload:', payload);
     return new Response(null, { status: 204 });
 }

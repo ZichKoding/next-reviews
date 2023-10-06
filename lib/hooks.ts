9cc73c56054a6f3bce83d5d37dab6480a55e6113
ChrisZichko <108543895+ChrisZichko@users.noreply.github.com>
2023-10-06 08:40:05 -0400

    added client-only rendering

---
+++ lib/hooks.ts
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+export function useIsClient(): boolean {
+    const [isClient, setClient] = useState(false);
+    useEffect(() => {
+        setClient(true);
+    }, []);
+    return isClient;
+}
